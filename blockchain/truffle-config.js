8ba2c11882d93b6d884371cd8ae78eaea3457ff6
Maddie Smith <54409681+maddie-aos@users.noreply.github.com>
2023-04-22 09:23:13 -0700

    commit

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -10,10 +10,15 @@ module.exports = {
     contracts_build_directory: './src/build/',
     compilers: {
       solc: {
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
+        version: "0.8.19",    // Fetch exact version from solc-bin (default: truffle's version)
+        // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+        // settings: {          // See the solidity docs for advice about optimization and evmVersion
+        //  optimizer: { 
+        //    enabled: false,
+        //    runs: 200
+        //  },
+        //  evmVersion: "byzantium"
+        // }
       }
-    }
+    },
 }
